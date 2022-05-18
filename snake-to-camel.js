8f1ef913a277c723f83642fd1906f42b3243627b
Tony Alvarez <tonyalva87@gmail.com>
2022-05-17 20:40:08 -0700

    completed star-out-grid and snake-to-camel-exercises

--- snake-to-camel.js
+++ snake-to-camel.js
@@ -1,2 +1,12 @@
-function snakeToCamel() {}
+function snakeToCamel(str) {
+	let split_str = str.split("_");
 
+	let result = split_str[0];
+
+	// start at index 1 because we don't want to affect first split str item (at 0-index)
+	for (let i = 1; i < split_str.length; i++) {
+		split_str[i] = split_str[i][0].toUpperCase() + split_str[i].slice(1);
+		result = result.concat(split_str[i]);
+	}
+	return result;
+}
--- star-out-grid.js
+++ star-out-grid.js
@@ -1,2 +1,32 @@
-function starOutGrid(grid) {
+function findStarCoordinates(grid, star) {
+	let star_locs = [];
+
+	// find the coordinates of stars and put them into array
+	for (let row of grid) {
+		let found_row;
+		let found_col;
+		if (row.indexOf(star) >= 0) {
+			found_row = grid.indexOf(row);
+			found_col = row.indexOf(star);
+			star_locs.push([found_row, found_col]);
+		}
+	}
+	return star_locs;
+}
+
+function starOutGrid(grid, star = "*") {
+	let star_locs = findStarCoordinates(grid, star);
+
+	for (let star_loc of star_locs) {
+		for (let [row_id, row] of grid.entries()) {
+			if (row_id == star_loc[0]) {
+				row.forEach((col, index) => {
+					row[index] = "*";
+				});
+			}
+			row[star_loc[1]] = "*";
+		}
+	}
+
+	return grid;
 }
